fc046ff7d085c0a1406f3020372b3575e45c32b5
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-05-18 18:27:40 +0530

    why choose content

---
+++ src/components/content/whyChoose.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Container,
+} from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+
+const WhyChooseUs = () => {
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        my: 8,
+      }}
+    >
+      <Typography
+        variant="h4"
+        gutterBottom
+        fontWeight="bold"
+        color="white"
+        align="center"
+      >
+        Why Choose Us
+      </Typography>
+      <Typography variant="body1" color="white" align="center" gutterBottom>
+        Looking for the perfect partner for your project? Look no further!
+        Here's why you should choose us:
+        <br />
+        <br />
+        Partner with us today and let's turn your vision into reality!
+      </Typography>
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <CheckIcon color="primary" />
+          </ListItemIcon>
+          <ListItemText
+            primary="Quality Solutions"
+            primaryTypographyProps={{ color: "white", fontWeight: "bold" }}
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <CheckIcon color="primary" />
+          </ListItemIcon>
+          <ListItemText
+            primary="Expert Guidance"
+            primaryTypographyProps={{ color: "white", fontWeight: "bold" }}
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <CheckIcon color="primary" />
+          </ListItemIcon>
+          <ListItemText
+            primary="Exceptional Support"
+            primaryTypographyProps={{ color: "white", fontWeight: "bold" }}
+          />
+        </ListItem>
+      </List>
+    </Container>
+  );
+};
+
+export default WhyChooseUs;
