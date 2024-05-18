0f2d77e3edd0b89aed1afd6ee2c4eda534beeb23
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-05-18 18:50:30 +0530

    main lanading page of the application

---
+++ src/layout/designShowcase.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Design from '../components/Gallery/design1'
+import Design2 from '../components/Gallery/design2'
+export default function designShowcase() {
+  return (
+    <div>
+      <Design/>
+      <Design2/>
+    </div>
+  )
+}
--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import Navbar from "../components/header/navbar"
-import VideoShowcase from '../components/videoShowcase'
-import IntroParagraph from '../components/introPara'
-import DesignShowcase from '../components/Gallery/design1'
-import DesignShowcase2 from '../components/Gallery/design2'
+import React from "react";
+import Navbar from "../components/header/navbar";
+import VideoShowcase from "../components/videoShowcase";
+import IntroParagraph from "../components/content/introPara";
+import WhyChooseUs from "../components/content/whyChoose";
+import DesignShowcase from "../layout/designShowcase";
+import Portfolio from "../components/portfolio/portfolio";
+import OurProcess from "../components/content/ourProcess";
+import Footer from "../components/footer";
+import ChatbotButton from "../components/chatBot";
 
 export default function landingPage() {
   return (
     <div>
-      <Navbar/>
-      <VideoShowcase/>
-      <IntroParagraph/>
-      <DesignShowcase/>
-      <DesignShowcase2/>
-
+      <Navbar />
+      <VideoShowcase />
+      <IntroParagraph />
+      <DesignShowcase />
+      <WhyChooseUs />
+      <Portfolio/>
+      <OurProcess/>
+      <Footer/>
+      <ChatbotButton/>
     </div>
-  )
+  );
 }
